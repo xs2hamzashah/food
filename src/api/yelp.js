b39efef1335d8065de3f476a327aa8e57ffddc3c
Hamza Shah <xs2hamzashah@gmail.com>
2024-05-29 10:09:03 +0500

    Added searchApi, handled error and modified SearchBar prop Values. and modified yelp api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses/",
   headers: {
-    Authorization: "Bearer API_key",
+    Authorization: "Bearer Jw0oIMgpId1HV8x-mogAapr36SVRDSAM00qOEvAmLyxCaOV1I0T6kzJbSvazjA6Q7sNS46uHfHzRzLLAESkHYv3ES50h-sUQwtwvh836OsN-D5UwO6ObMswyxDM6YXYx",
   },
 });
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,42 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "./components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
-  const [term, setTerm] = useState('');
-  console.log('term: ', term);
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]); // will store the yelp
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    console.log("Hi, there");
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage("Something went wrong.");
+    }
+  };
+
+  // Call serachApi when component
+  // is first rerendered. BAD CODE!
+  //searchApi('pasta');
+
   return (
     <View>
-      <SearchBar 
-        term={term} 
-        onTermChange={(newTerm)=> setTerm(newTerm)}
-        onTermSubmit={()=> console.log('submitted!.')}  
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>we have found {results.length} results</Text>
     </View>
   );
 };
